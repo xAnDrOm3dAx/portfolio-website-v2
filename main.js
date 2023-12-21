3851b88e0fbf1f9c959f9d7b9bbe9206916544a1
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-12-22 08:33:27 +1000

    refine logic to handle light/dark

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const themeToggleLight = document.querySelector("#theme-toggle-light");
+const themeToggleDark = document.querySelector("#theme-toggle-dark");
 
 let darkMode = localStorage.getItem("darkMode");
-
 if (darkMode === null) {
   darkMode = "disabled";
   localStorage.setItem("darkMode", darkMode);
 }
 
-darkModeToggle.addEventListener("click", () => {
-  darkMode = localStorage.getItem("darkMode");
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
-});
-
 const enableDarkMode = () => {
+  themeToggleLight.style.display = "block";
+  themeToggleDark.style.display = "none";
   document.documentElement.classList.remove("light");
   document.documentElement.classList.add("dark");
   localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
+  themeToggleLight.style.display = "none";
+  themeToggleDark.style.display = "block";
   document.documentElement.classList.remove("dark");
   document.documentElement.classList.add("light");
   localStorage.setItem("darkMode", "disabled");
@@ -30,4 +25,29 @@ const disableDarkMode = () => {
 
 if (darkMode === "enabled") {
   enableDarkMode();
+} else {
+  themeToggleLight.style.display = "none";
+  themeToggleDark.style.display = "block";
 }
+
+themeToggleLight.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  disableDarkMode();
+  // if (darkMode !== "enabled") {
+  //   enableDarkMode();
+  // } else {
+  //   disableDarkMode();
+  // }
+});
+
+themeToggleDark.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  enableDarkMode();
+  // if (darkMode !== "enabled") {
+  //   enableDarkMode();
+  // } else {
+  //   disableDarkMode();
+  // }
+});
+
+console.log(darkMode);
