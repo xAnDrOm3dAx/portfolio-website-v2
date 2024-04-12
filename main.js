e73a86eb6e51bd3e4690c0fc85d255adc28cd7ea
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-04-12 11:41:13 +1000

    add observer for scrolling animations

--- main.js
+++ main.js
@@ -1,45 +1,3 @@
-// const themeToggleLight = document.querySelector("#theme-toggle-light");
-// const themeToggleDark = document.querySelector("#theme-toggle-dark");
-
-// let darkMode = localStorage.getItem("darkMode");
-// if (darkMode === null) {
-//   darkMode = "disabled";
-//   localStorage.setItem("darkMode", darkMode);
-// }
-
-// if (darkMode === "enabled") {
-//   enableDarkMode();
-// } else {
-//   themeToggleLight.style.display = "none";
-//   themeToggleDark.style.display = "flex";
-// }
-
-// function enableDarkMode() {
-//   themeToggleLight.style.display = "flex"; // Hide sun icon
-//   themeToggleDark.style.display = "none"; // Show moon icon
-//   document.documentElement.classList.remove("light");
-//   document.documentElement.classList.add("dark");
-//   localStorage.setItem("darkMode", "enabled");
-// }
-
-// function disableDarkMode() {
-//   themeToggleLight.style.display = "none"; // Show sun icon
-//   themeToggleDark.style.display = "flex"; // Hide moon icon
-//   document.documentElement.classList.remove("dark");
-//   document.documentElement.classList.add("light");
-//   localStorage.setItem("darkMode", "disabled");
-// }
-
-// themeToggleLight.addEventListener("click", () => {
-//   disableDarkMode();
-// });
-
-// themeToggleDark.addEventListener("click", () => {
-//   enableDarkMode();
-// });
-
-// console.log(darkMode);
-
 const themeToggleLight = document.querySelector("#theme-toggle-light");
 const themeToggleDark = document.querySelector("#theme-toggle-dark");
 
@@ -86,4 +44,31 @@ themeToggleDark.addEventListener("click", () => {
   enableDarkMode();
 });
 
-console.log(darkMode);
+//
+//
+//
+//
+//  Intersection Observer for scrolling animations
+const observer = new IntersectionObserver(
+  (animations) => {
+    animations.forEach((element) => {
+      if (element.isIntersecting) {
+        element.target.classList.add("animate");
+        element.target.classList.remove("undo-animate");
+      } else {
+        element.target.classList.remove("animate");
+        element.target.classList.add("undo-animate");
+      }
+    });
+  },
+  {
+    rootMargin: "0px",
+    threshold: [0, 0.2, 1],
+  }
+);
+
+const tags = document.querySelectorAll(".project-wrapper, section");
+
+tags.forEach((tag) => {
+  observer.observe(tag);
+});
